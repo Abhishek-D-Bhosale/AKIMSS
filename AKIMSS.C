ddd3ceac7cbcf140340d509bdd874d0e34462f91
samcyab <89337354+samcyab@users.noreply.github.com>
2022-12-16 13:07:37 +0530

    First Committ

---
+++ AKIMSS.C
@@ -0,0 +1,174 @@
+#include <stdio.h>
+
+void main ()
+
+{
+   int x, y, z, a, b;
+   clrscr();
+   printf(" \t\t\t *************************** ");
+   printf("\n\t\t\t\t\ AKIMSS \n");
+   printf(" \t\t\t *************************** ");
+   printf("\nwelcome to AKIMSS ! \nwe here by conduct four main courses.");
+   printf("\n================================================================================ \n");
+   printf("Select Courses : \n 1.For BCA \n 2.For BBA \n 3.For MCA\n 4.For MBA \n 5.To exit \n");
+   printf("\n================================================================================ \n");
+   printf("College Management system Application\n");
+   printf("Project By :~Abhishek Bhosale \n");
+   scanf("%d",&x);
+
+   switch(x)
+
+   {
+      case 1 :
+	 printf("\n================================================================================ ");
+	 printf("You've selected BCA course.");
+	 printf("\nBCA full form is Bachelor of Computer Applications.");
+	 printf("BCA Course is a 3 year, full-time undergraduate course that studies implementation for computer applications in software\n");
+	 printf("\nBCA Full Form            :Bachelor of Computer Applications\nBCA Duration	         :3 years\nBCA Eligibility	         :Minimum 50% marks in 10+2 \nBCA Admission Process    :Either by Entrance Exams or by Merit\nBCA Course Fees	         :Up to 2 to 3 Lakhs\n");
+	 printf("\n1. SEM I\n2. SEM II\n3. SEM III\n4. SEM IV\n5. SEM V\n6. SEM VI\n ");
+	 printf("\n================================================================================ ");
+
+
+	 scanf("%d",&y);
+	 switch(y)
+	 {
+
+		case 1:
+		printf("\n================================================================================ \n");
+		printf("The BCA course structure is spread across six semsters in three years.Each semester comprises of seven papers with a balance combination of theory and practicalThe course continent is regular updated to keep peace chances in the business enviornment\n");
+		printf("\nTotal fees payable at the time of admission          : 32500\nFees per year                                        : 30000\nFees payable for each semester exam	             : 3500 Apx.\n");
+		printf("\nSemester first contains following subjects\n");
+		printf("\n- APD \n- C Pro \n- BOS \n- BM \n- FIT \n- C programming Practical \n- MS Office");
+		printf("\n\n================================================================================ \n");
+		printf("\nTo exit press enter \n ");
+		break;
+		case 2:
+		printf("\n\n================================================================================ \n");
+		printf("The BCA course structure is spread across six semsters in three years.Each semester comprises of seven papers with a balance combination of theory and practicalThe course continent is regular updated to keep peace chances in the business enviornment\n");
+		printf("\nTotal fees payable at the time of admission          : 32500\nFees per year                                        : 30000\nFees payable for each semester exam	             : 3500 Apx.\n");
+		printf("\nSemester second contains following subjects\n");
+		printf("\n- POM \n- COA \n- DBMS \n- FA \n- C-II \n- C-II programming Practical");
+		printf("\n\n================================================================================ \n");
+		printf("\nTo exit press enter \n ");
+		break;
+		case 3:
+		printf("\n\n================================================================================ \n");
+		printf("The BCA course structure is spread across six semsters in three years.Each semester comprises of seven papers with a balance combination of theory and practicalThe course continent is regular updated to keep peace chances in the business enviornment\n");
+		printf("\nTotal fees payable at the time of admission          : 32500\nFees per year                                        : 30000\nFees payable for each semester exam	             : 3500 Apx.\n");
+		printf("\nSemester third contains following subjects\n");
+		printf("\n- Statics \n- SQL \n- DBMS-II \n- SE \n- OS \n- MT");
+		printf("\n\n================================================================================ \n");
+		printf("\nTo exit press enter \n ");
+		break;
+		case 4:
+		printf("\n\n================================================================================ \n");
+		printf("The BCA course structure is spread across six semsters in three years.Each semester comprises of seven papers with a balance combination of theory and practicalThe course continent is regular updated to keep peace chances in the business enviornment\n");
+		printf("\nTotal fees payable at the time of admission          : 32500\nFees per year                                        : 30000\nFees payable for each semester exam	             : 3500 Apx.\n");
+		printf("\nSemester Fourth contains following subjects\n");
+		printf("\n- CN \n- JP \n- OR \n- ST \n- ED \n- LAB On Java");
+		printf("\n\n================================================================================ \n");
+		printf("\nTo exit press enter \n ");
+		break;
+		case 5:
+		printf("\n\n================================================================================ \n");
+		printf("The BCA course structure is spread across six semsters in three years.Each semester comprises of seven papers with a balance combination of theory and practicalThe course continent is regular updated to keep peace chances in the business enviornment\n");
+		printf("\nTotal fees payable at the time of admission          : 32500\nFees per year                                        : 30000\nFees payable for each semester exam	             : 3500 Apx.\n");
+		printf("\nSemester fifth contains following subjects\n");
+		printf("\n- IT \n- OOAD \n- C# \n- GT \n- E1 \n- Lab on C# and I");
+		printf("\n\n================================================================================ \n");
+		printf("\nTo exit press enter \n ");
+		break;
+		case 6:
+		printf("\n\n================================================================================ \n");
+		printf("The BCA course structure is spread across six semsters in three years.Each semester comprises of seven papers with a balance combination of theory and practicalThe course continent is regular updated to keep peace chances in the business enviornment\n");
+		printf("\nTotal fees payable at the time of admission          : 32500\nFees per year                                        : 30000\nFees payable for each semester exam	             : 3500 Apx.\n");
+		printf("\nSemester sixth contains following subjects\n");
+		printf("\n- DWDM \n- WP \n- SPM \n- BA \n- E-II");
+		printf("\n\n================================================================================ \n");
+		printf("\nTo exit press enter \n ");
+		break;
+		default: printf(" you've selected Invalid SEM");
+	 }
+	 break;
+	 case 2 :
+	 printf(" \n You've selected BBA course .\n ");
+	 printf("\n1. SEM 1\n2. SEM 2\n3. SEM 3\n4. SEM 4\n5. SEM 5\n6. SEM 6\n ");
+	 scanf("%d",&z);
+	 switch(z)
+	 {
+		case 1:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 2:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 3:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 4:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 5:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 6:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		default: printf(" you've selected Invalid SEM");
+	 }
+	 break;
+	case 3 :
+	 printf(" \n You've selected MCA course .\n ");
+	 printf("\n1. SEM 1\n2. SEM 2\n3. SEM 3\n4. SEM 4\n");
+	 scanf("%d",&a);
+	 switch(a)
+	 {
+		case 1:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 2:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 3:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 4:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		default: printf(" you've selected Invalid SEM");
+	 }
+	 break;
+	 case 4:
+	 printf(" \n You've selected MCA course .\n ");
+	 printf("\n1. SEM 1\n2. SEM 2\n3. SEM 3\n4. SEM 4\n");
+	 scanf("%d",&b);
+	 switch(b)
+	 {
+		case 1:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 2:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 3:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		case 4:
+		printf("\n- I \n- II \n- III \n- IV \n- V \n- VI \n- VII");
+		break;
+		default: printf(" you've selected Invalid SEM");
+	 }
+	 break;
+	 case 5:
+	 {
+		return 0;
+	 }
+	 break;
+
+
+	default: printf(" you've selected another fileds");
+   }
+   getch();
+
+
+}
